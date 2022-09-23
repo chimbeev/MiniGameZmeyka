aa613dc204dad65be4f207cfca268e6e66e66a18
Chimbeev <chimbeevaa@gmail.com>
2022-09-23 09:08:56 +0800

    230922 1

---
+++ Zmeyka.js
@@ -0,0 +1,12 @@
+class Cell {
+    constructor(x, y, color ) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    getColor(x,y) { //Получить цвет клетки
+
+    }
+
+
+}
